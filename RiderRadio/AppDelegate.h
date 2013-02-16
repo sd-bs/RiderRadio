8210cb98afc1b4d28c3a8f61f1946176890e16b1
Fabien MOUSSAVI <fabien@kickyourapp.com>
2013-02-16 22:48:06 +0100

    Initial Commit

---
+++ RiderRadio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RiderRadio
+//
+//  Created by Fabien Moussavi on 16/02/13.
+//  Copyright (c) 2013 Fabien Moussavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RiderRadio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RiderRadio
+//
+//  Created by Fabien Moussavi on 16/02/13.
+//  Copyright (c) 2013 Fabien Moussavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
